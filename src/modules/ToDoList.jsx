2d18a16db3f0babf348e8033c6327e05ec97006d
jerryni <jerryni2014@gmail.com>
2017-10-27 17:17:04 +0800

    Add husky

--- src/modules/ToDoList.jsx
+++ src/modules/ToDoList.jsx
@@ -1,85 +1,83 @@
 import React from 'react'
 
 class ToDoList extends React.Component {
-    
-    constructor(props) {
-      super(props)
-      this.state = {
-        text: props.text || ''
-      }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      text: props.text || ''
     }
+  }
 
-    static defaultProps = {
-      todos: []
-    }
+  static defaultProps = {
+    todos: []
+  }
 
-    handleKeyup = e => {
-      if(e.key === 'Enter'){
-        let value = e.target.value.trim()
+  handleKeyup = e => {
+    if (e.key === 'Enter') {
+      let value = e.target.value.trim()
 
-        this.props.todos.push({
-          name: value,
-          id: new Date().getTime(),
-          completed: false
-        })
+      this.props.todos.push({
+        name: value,
+        id: new Date().getTime(),
+        completed: false
+      })
 
-        this.setState({
-          text: ''
-        })
-
-        console.log(this.props.todos)
-      }
-    }
-
-    handleChange = e => {
       this.setState({
-        text: e.target.value
+        text: ''
       })
     }
+  }
 
-    handleRemoveTodo = index => {
-      this.props.todos.splice(index, 1)
+  handleChange = e => {
+    this.setState({
+      text: e.target.value
+    })
+  }
 
-      this.setState(this.state)
-    }
+  handleRemoveTodo = index => {
+    this.props.todos.splice(index, 1)
 
-    onToggle = todo => {
-      todo.completed = !todo.completed
-      this.setState(this.state)
-    }
+    this.setState(this.state)
+  }
 
-    onToggleAll = e => {
-      let {checked} = e.target
-      this.props.todos.forEach(todo => {
-        todo.completed = checked
-      })
-      this.setState(this.state)
-    }
+  onToggle = todo => {
+    todo.completed = !todo.completed
+    this.setState(this.state)
+  }
 
-    render() {
-        let {todos} = this.props
-        return (
-          <div>
-            <input type="checkbox" onChange={this.onToggleAll}/>
-            <input type="text"
-            onKeyUp={this.handleKeyup}
-            onChange={this.handleChange}
-            value={this.state.text}/>
-            <ul>
-              {todos.map( (todo, index) => 
-                <li key={todo.id}>
-                  <label>
-                  <input type="checkbox" checked={todo.completed}
-                    onChange={()=> {this.onToggle(todo)}}/>
-                  {todo.name}
-                  </label>
+  onToggleAll = e => {
+    let { checked } = e.target
+    this.props.todos.forEach(todo => {
+      todo.completed = checked
+    })
+    this.setState(this.state)
+  }
 
-                  <span className="close" onClick={() => {this.handleRemoveTodo(index)}}>X</span>
-                </li>
-              )}
-            </ul>
-          </div>
-        )
-    }
+  render() {
+    let { todos } = this.props
+    return (
+      <div>
+        <input type="checkbox" onChange={this.onToggleAll} />
+        <input type="text"
+          onKeyUp={this.handleKeyup}
+          onChange={this.handleChange}
+          value={this.state.text} />
+        <ul>
+          {todos.map((todo, index) =>
+            <li key={todo.id}>
+              <label>
+                <input type="checkbox" checked={todo.completed}
+                  onChange={() => { this.onToggle(todo) }} />
+                {todo.name}
+              </label>
+
+              <span className="close" onClick={() => { this.handleRemoveTodo(index) }}>X</span>
+            </li>
+          )}
+        </ul>
+      </div>
+    )
+  }
 }
 export default ToDoList
